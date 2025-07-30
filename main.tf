09ca5373f44cc7addf24a63a8089e72e548ea991
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-07-30 19:11:35 +0530

    eks cluster setup

--- main.tf
+++ main.tf
@@ -15,8 +15,10 @@ module "vpc" {
 module "eks" {
   source                  = "git::https://github.com/teju-devopss/tf-module-eks.git"
   for_each                = var.eks
+
+
   env                     =  var.env
   project_name            = var.project_name
   component               = "eks"
-  subnets_ids             = lookup(lookup(module.vpc,"main",null),"app_subnets_cidr",null)
+  subnets_ids             = lookup(lookup(module.vpc,"main",null),"app_subnets_ids",null)
 }
