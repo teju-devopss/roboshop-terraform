2abc30b326f75f453b1ec351331495fa5b0ccbd3
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-07-31 17:57:16 +0530

    parking rabbitmq aside

--- main.tf
+++ main.tf
@@ -66,24 +66,24 @@ module "elasticache" {
 
 }
 
-module "rabbitmq" {
- source = "git::https://github.com/teju-devopss/tf-module-rabbitmq.git"
-
- for_each      = var.rabbitmq
- instance_type = each.value["instance_type"]
-
- env             = var.env
- tags            = {}
- kms             = var.kms_key_id
- bastion_cidrs   = var.bastion_cidrs
- route53_zone_id = var.zone_id
- project_name = var.project_name
-
- subnets  = lookup(lookup(module.vpc, "main", null), "db_subnets_ids", null)
- vpc_id   = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
- sg_cidrs = lookup(lookup(var.vpc, "main", null), "app_subnets_cidr", null)
-
-}
+# module "rabbitmq" {
+#  source = "git::https://github.com/teju-devopss/tf-module-rabbitmq.git"
+#
+#  for_each      = var.rabbitmq
+#  instance_type = each.value["instance_type"]
+#
+#  env             = var.env
+#  tags            = {}
+#  kms             = var.kms_key_id
+#  bastion_cidrs   = var.bastion_cidrs
+#  route53_zone_id = var.zone_id
+#  project_name = var.project_name
+#
+#  subnets  = lookup(lookup(module.vpc, "main", null), "db_subnets_ids", null)
+#  vpc_id   = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+#  sg_cidrs = lookup(lookup(var.vpc, "main", null), "app_subnets_cidr", null)
+#
+# }
 
 module "rds" {
  source = "git::https://github.com/teju-devopss/tf-module-rds.git"
